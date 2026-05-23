e8700b4c6459e07c09b4eea047570030b75a5681
habibanadeemhere <habibanadeem075@gmail.com>
2026-05-23 02:46:47 -0700

    frontend update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://to-do-application-frontend-phi.vercel.app/api",
+  baseURL: "https://to-do-application-backend-five.vercel.app/api",
 });
 
 API.interceptors.request.use((req) => {
