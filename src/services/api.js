9267fc2c6dd79a8bb76117858665535a4ea04404
habibanadeemhere <habibanadeem075@gmail.com>
2026-05-30 00:31:42 -0700

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://to-do-application-backend-jtjf.vercel.app/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-API.interceptors.request.use(
-  (config) => {
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-);
+
+  return config;
+});
 
 export default API;
